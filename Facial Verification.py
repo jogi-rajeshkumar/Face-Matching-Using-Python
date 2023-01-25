c5819ba6e1ce410d7a04a5ad297ea678a397a371
JOGI RAJESH KUMAR <119765432+jogi-rajeshkumar@users.noreply.github.com>
2023-01-25 11:10:17 +0530

    Update Facial Verification.py

--- Facial Verification.py
+++ Facial Verification.py
@@ -1,10 +1,10 @@
 import face_recognition
 import os
 import cv2
-source_img = face_recognition.load_image_file("//home//rajesh//Desktop//week3//TASK 4//Images//295999c1c9187d059a31dfaf4a02828b.jpg")
+source_img = face_recognition.load_image_file("Path to Source Image")
 source_face_encoding = face_recognition.face_encodings(source_img)[0]
-for filename in os.listdir("//home//rajesh//Desktop//week3//TASK 4//Images"):
-    img = face_recognition.load_image_file(f"//home//rajesh//Desktop//week3//TASK 4//Images//{filename}")
+for filename in os.listdir("Path to Directory"):
+    img = face_recognition.load_image_file(f"Path to Directory{filename}")
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     img_face_encoding = face_recognition.face_encodings(img)[0]
     results = face_recognition.compare_faces([source_face_encoding], img_face_encoding)
